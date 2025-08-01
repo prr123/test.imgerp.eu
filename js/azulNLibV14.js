52eabb30a05d8ebfdac160612098cca6ca735155
Peter <azulpeter@azulcloud2.tail1b332.ts.net>
2025-08-01 16:20:21 +0200

    small changes

--- js/azulNLibV14.js
+++ js/azulNLibV14.js
@@ -321,6 +321,29 @@ let azul = {
 		}
 		pardiv.appendChild(chdiv);
 	},
+
+	isAlphaNumeric(str) {
+  		for (let i = 0; len = str.length; i < len; i++) {
+    		code = str.charCodeAt(i);
+    		if (!(code > 47 && code < 58) && // numeric (0-9)
+        		!(code > 64 && code < 91) && // upper alpha (A-Z)
+        		!(code > 96 && code < 123)) { // lower alpha (a-z)
+      				return false;
+    			}
+  			}
+		return true;
+	},
+
+	isAlpha(str) {
+  		for (let i = 0; len = str.length; i < len; i++) {
+    		code = str.charCodeAt(i);
+    		if (!(code > 64 && code < 91) && // upper alpha (A-Z)
+        		!(code > 96 && code < 123)) { // lower alpha (a-z)
+      				return false;
+    			}
+  			}
+		return true;
+	},
 };
 
 class AzulBlog {
